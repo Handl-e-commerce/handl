759bc09b50e4d1b2ad9f26a6bee7437cbee5d5ce
Aaron Isakov <Aaronisakov@gmail.com>
2024-06-14 14:41:38 -0500

    Created Vendor model class and its respective migration file.

---
+++ backend/db/migrations/20240614192859-create-vendors.ts
@@ -0,0 +1,65 @@
+import {DataTypes, QueryInterface} from "sequelize";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  // eslint-disable-next-line
+  async up(queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
+    await queryInterface.createTable('Vendors', {
+      uuid: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        allowNull: false,
+        unique: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      description: {
+        type: DataTypes.STRING(5000),
+        allowNull: true
+      },
+      website: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      categories: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: true
+      },
+      people: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: true
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      city: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      state: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      zipcode: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      phoneNumber: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true
+      }
+    });
+  },
+  // eslint-disable-next-line
+  async down(queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
+    await queryInterface.dropTable('Vendors');
+  }
+};
---
+++ backend/db/models/Vendor.ts
@@ -0,0 +1,76 @@
+import {Model, InferAttributes, InferCreationAttributes, DataTypes} from "sequelize";
+import {Database} from "../Database";
+
+class Vendor extends Model<InferAttributes<Vendor>, InferCreationAttributes<Vendor>> {
+  declare uuid: string;
+  declare name: string;
+  declare description: string;
+  declare website: string;
+  declare categories: string; 
+  declare people: string;
+  declare address: string; 
+  declare city: string; 
+  declare state: string; 
+  declare zipcode: string; 
+  declare phoneNumber: string; 
+  declare email: string; 
+}
+
+Vendor.init({
+  uuid: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+    allowNull: false,
+    unique: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  description: {
+    type: DataTypes.STRING(5000),
+    allowNull: true
+  },
+  website: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  categories: {
+    type: DataTypes.ARRAY(DataTypes.STRING),
+    allowNull: true
+  },
+  people: {
+    type: DataTypes.ARRAY(DataTypes.STRING),
+    allowNull: true
+  },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  state: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  zipcode: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  phoneNumber: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
+}, {
+  sequelize: Database.GetInstance().sequelize,
+  modelName: 'Vendors',
+});
+
+export {Vendor}
