acf8006af81df93032bb739164f9135618a128c7
Aaron Isakov <Aaronisakov@gmail.com>
2024-07-27 16:12:29 -0500

    Implemented typing logic for 2 of the functions.

--- frontend/src/routes/SignUp/SignUp.tsx
+++ frontend/src/routes/SignUp/SignUp.tsx
@@ -24,23 +24,21 @@ function SignUp(): JSX.Element {
     const [password, setPassword] = useState<string>("");
     const [signUpSuccess, setSignUpSuccess] = useState<boolean>();
 
-    // TODO: (LOW) Add proper typing for event argument inputs
-    const isNumericInput = (event: any) => {
-        const key = event.keyCode;
-        return ((key >= 48 && key <= 57) || // Allow number line
-            (key >= 96 && key <= 105) // Allow number pad
-        );
+    const isNumericInput = (event: KeyboardEvent) => {
+        const key = event.key;
+        return ((key >= "0" && key <= "9"));
     };
     
-    const isModifierKey = (event: any) => {
-        const key = event.keyCode;
-        return (event.shiftKey === true || key === 35 || key === 36) || // Allow Shift, Home, End
-            (key === 8 || key === 9 || key === 13 || key === 46) || // Allow Backspace, Tab, Enter, Delete
-            (key > 36 && key < 41) || // Allow left, up, right, down
+    const isModifierKey = (event: KeyboardEvent) => {
+        const key = event.key;
+        console.log(key);
+        return (event.shiftKey === true || key === "Home" || key === "End") || // Allow Shift, Home, End
+            (key === "Backspace" || key === "Tab" || key === "Enter" || key === "Delete") || // Allow Backspace, Tab, Enter, Delete
+            (key === "ArrowLeft" || key === "ArrowRight" || key === "ArrowUp" || key === "ArrowDown") || // Allow left, up, right, down
             (
                 // Allow Ctrl/Command + A,C,V,X,Z
                 (event.ctrlKey === true || event.metaKey === true) &&
-                (key === 65 || key === 67 || key === 86 || key === 88 || key === 90)
+                (key === "a" || key === "c" || key === "v" || key === "x" || key === "z")
             )
     };
     
