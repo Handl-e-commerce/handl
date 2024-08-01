595dc9c6c727c01faf73321e032bc140304a5d29
Aaron Isakov <Aaronisakov@gmail.com>
2024-08-01 18:27:54 -0500

    Created some basic row that needs to be tested.

--- frontend/src/components/ExpandedRow/ExpandedRow.tsx
+++ frontend/src/components/ExpandedRow/ExpandedRow.tsx
@@ -1,42 +1,38 @@
 import React from "react";
 import { vendorRow } from "../../types/types";
 
-interface ExpandedRowProps {
+interface IExpandedRowProps {
     row: vendorRow;
     ids: string[];
 };
 
-function ExpandedRow(props: ExpandedRowProps): JSX.Element {
-    return (
-        <>
-            {props.ids.includes(props.row.id) && (
-              <tr style={{ display: "flex", gridColumn: "1 / -1" }}>
-                <td style={{ flex: "1" }}>
-                  <ul
-                    style={{
-                      margin: "0",
-                      padding: "0",
-                      backgroundColor: "#e0e0e0",
-                    }}
-                  >
-                    <li>
-                      <strong>Name:</strong> {props.row.name.toUpperCase()}
-                    </li>
-                    <li>
-                      <strong>Address:</strong> {props.row.address}
-                    </li>
-                    <li>
-                      <strong>Description:</strong> {props.row.description}
-                    </li>
-                    <li>
-                      <strong>People:</strong> {props.row.people}
-                    </li>
-                  </ul>
-                </td>
-              </tr>
-            )}
-        </>
-    );
+function ExpandedRow({row, ids}: IExpandedRowProps): JSX.Element {
+    
+  return (
+    <>
+        {ids.includes(row.id) && (
+          <tr style={{ display: "flex", gridColumn: "1 / -1" }}>
+            <td style={{ flex: "1" }}>
+              <div>
+                <h4>Company Information</h4>
+                <div>Name: {row.name.toUpperCase()}</div>
+                <div>Description: {row.description}</div>
+                <div>Website: {row.website}</div>
+                <div>Categories: {row.categories}</div>
+                <h4>Contact Info</h4>
+                {row.people.map((person) => {
+                  return <div>People: {person}</div>
+                })}
+                <div>Email: {row.email}</div>
+                <div>Phone Number: {row.phoneNumber}</div>
+                <div>{row.address + ", " + row.city}</div>
+                <div>{row.state + ", " + row.zipcode}</div>
+              </div>
+            </td>
+          </tr>
+        )}
+    </>
+  );
 };
 
 export { ExpandedRow };
