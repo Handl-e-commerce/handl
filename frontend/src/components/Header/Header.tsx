f4781a7dc2fc3f944a425531892e5563175adf53
Aaron Isakov <Aaronisakov@gmail.com>
2024-08-25 23:25:34 -0500

    Implemented MUI components in header. Tests are super slow now, need to investigate if it's because MUI slows jest down.

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { SearchBar } from "../SearchBar/SearchBar";
 import { fetchWrapper } from "../../utils/fetch-wrapper";
 import { cookieParser, deleteCookie } from "../../utils/cookie-util";
 import { useLoginStatus } from "../../hooks/useLoggedInStatus";
+import { Box, Button, Menu, MenuItem } from "@mui/material";
+import { KeyboardArrowDown, Logout } from "@mui/icons-material";
+import LogoutIcon from '@mui/icons-material/Logout';
 import svg from '../../static/5_SVG.svg';
 
 const envVariables = process.env;
@@ -12,7 +15,8 @@ const {
 
 function Header(): JSX.Element {
     const cookieObject = cookieParser();
-    let location = window.location;
+    const location = window.location;
+    const [menuAnchor, setMenuAnchor] = useState<null | HTMLElement>(null);
     let isLandingPage: boolean = location.pathname === "/";
     let isLoginOrSignUpPage: boolean = location.pathname === "/login" || location.pathname === "/sign-up";
     let loggedIn = useLoginStatus();
@@ -36,18 +40,46 @@ function Header(): JSX.Element {
         redirectLogin();
     };
 
+    function handleDropdownClick(event: React.MouseEvent<HTMLElement>): void {
+        setMenuAnchor(event.currentTarget);
+    };
+
+    function handleDropdownClose(): void {
+        setMenuAnchor(null);
+    };
+
     return (
         <header data-testid='header'>
             <a href={location.origin} target="_self">
                 <img src={svg} alt="Handl Logo" width={"250px"} height={"250px"}/>
             </a>
             {isLandingPage && <SearchBar />}
-            {loggedIn && <>
-                <div>Hi, {cookieObject?.firstName}!</div>
-                <div onClick={handleLogout}>Logout</div>
-            </>}
-            {!isLoginOrSignUpPage && !loggedIn && <button onClick={redirectSignUp}>Sign Up</button>}
-            {!isLoginOrSignUpPage && !loggedIn && <button onClick={redirectLogin}>Login</button>}
+            {loggedIn && 
+                <Box>
+                    <Button
+                        onClick={handleDropdownClick}
+                        endIcon={<KeyboardArrowDown />}
+                    >
+                        Hi, {cookieObject?.firstName}!
+                    </Button>
+                    <Menu
+                        anchorEl={menuAnchor}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                        }}
+                        open={Boolean(menuAnchor)}
+                        onClose={handleDropdownClose}
+                    >
+                        <MenuItem onClick={handleLogout}>
+                            Logout
+                            <Logout/>
+                        </MenuItem>
+                    </Menu>
+                </Box>
+            }
+            {!isLoginOrSignUpPage && !loggedIn && <Button variant="contained" onClick={redirectSignUp}>Sign Up</Button>}
+            {!isLoginOrSignUpPage && !loggedIn && <Button variant="outlined" onClick={redirectLogin}>Login</Button>}
         </header>
     );
 };
