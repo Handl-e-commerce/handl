36463bc5c8a80bddaa71b5b803ce5b98373d6972
Aaron Isakov <Aaronisakov@gmail.com>
2024-09-06 20:35:25 -0500

    Added enhanced table head but it needs a ton of work to get working.

---
+++ frontend/src/components/Table/EnhancedTableHead.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Vendor } from "../../types/types";
+import { TableHead, TableRow, TableCell,  } from "@mui/material";
+
+interface IEnhancedTableProps {
+    numSelected: number;
+    onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Vendor) => void;
+    onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    order: Vendor;
+    orderBy: string;
+    rowCount: number;
+};
+
+
+interface HeadCell {
+    disablePadding: boolean;
+    id: keyof Vendor;
+    label: string;
+    numeric: boolean;
+};
+  
+const headCells: readonly HeadCell[] = [
+    {
+      id: 'name',
+      numeric: false,
+      disablePadding: true,
+      label: 'Name',
+    },
+    {
+      id: 'categories',
+      numeric: false,
+      disablePadding: false,
+      label: 'Categories',
+    },
+    {
+      id: 'state',
+      numeric: false,
+      disablePadding: false,
+      label: 'State',
+    },
+];
+  
+
+function EnhancedTableHead({ onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort }: IEnhancedTableProps) {
+    const createSortHandler =
+      (property: keyof Vendor) => (event: React.MouseEvent<unknown>) => {
+        onRequestSort(event, property);
+      };
+  
+    return (
+      <TableHead>
+        <TableRow>
+          {headCells.map((headCell) => (
+            <TableCell
+              key={headCell.id}
+              align={headCell.numeric ? 'right' : 'left'}
+              padding={headCell.disablePadding ? 'none' : 'normal'}
+              sortDirection={orderBy === headCell.id ? order : false}
+            >
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : 'asc'}
+                onClick={createSortHandler(headCell.id)}
+              >
+                {headCell.label}
+                {orderBy === headCell.id ? (
+                  <Box component="span" sx={visuallyHidden}>
+                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                  </Box>
+                ) : null}
+              </TableSortLabel>
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+    );
+  }
--- frontend/src/components/Table/Test.tsx
+++ frontend/src/components/Table/Test.tsx
@@ -2,18 +2,59 @@ import React from "react";
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, SxProps } from "@mui/material";
 import { Vendor } from "../../types/types";
 
+type Order = 'asc' | 'desc';
+
 interface ITableProps {
     isMobile: boolean;
     data: Vendor[];
 };
 
 function Test({isMobile, data}: ITableProps): JSX.Element {
+    const [order, setOrder] = React.useState<Order>('asc');
+    const [orderBy, setOrderBy] = React.useState<keyof Vendor>('name');
+    const [selected, setSelected] = React.useState<readonly number[]>([]);
+    const [page, setPage] = React.useState(0);
+    const [dense, setDense] = React.useState(false);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    
+    function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+        if (b[orderBy] < a[orderBy]) {
+          return -1;
+        }
+        if (b[orderBy] > a[orderBy]) {
+          return 1;
+        }
+        return 0;
+    }
+      
+    function getComparator<Key extends keyof any>(
+        order: Order,
+        orderBy: Key,
+    ): (
+        a: { [key in Key]: number | string },
+        b: { [key in Key]: number | string },
+    ) => number {
+        return order === 'desc' ? (a, b) => descendingComparator(a, b, orderBy) : (a, b) => -descendingComparator(a, b, orderBy);
+    };
+
+    function stableSort<T>(array: readonly T[], comparator: (a: T, b: T) => number) {
+        const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+        stabilizedThis.sort((a, b) => {
+          const order = comparator(a[0], b[0]);
+          if (order !== 0) {
+            return order;
+          }
+          return a[1] - b[1];
+        });
+        return stabilizedThis.map((el) => el[0]);
+    };
+
     const tableContainerSx: SxProps = {
         overflowY: 'scroll',
         maxHeight: '90%',
         maxWidth: '95%',
         background: '#F2F2F7',
-    }
+    };
 
     const tableHeadSx: SxProps = {
         background: '#363636',
@@ -21,7 +62,7 @@ function Test({isMobile, data}: ITableProps): JSX.Element {
             color: 'white',
             fontWeight: 'bold',
         },
-    }
+    };
 
     return (
         <TableContainer className="HideScrollbar" component={Paper} sx={tableContainerSx}>
