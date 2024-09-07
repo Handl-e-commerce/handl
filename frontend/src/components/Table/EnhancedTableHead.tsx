73f8665b92a893ab58d882fce94cbb2caa2fbdc2
Aaron Isakov <Aaronisakov@gmail.com>
2024-09-06 21:43:31 -0500

    Resolved errors

--- frontend/src/components/Table/EnhancedTableHead.tsx
+++ frontend/src/components/Table/EnhancedTableHead.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Vendor } from "../../types/types";
-import { TableHead, TableRow, TableCell,  } from "@mui/material";
+import { TableHead, TableRow, TableCell, TableSortLabel, Box } from "@mui/material";
+import { visuallyHidden } from '@mui/utils';
+
+type Order = 'asc' | 'desc';
 
 interface IEnhancedTableProps {
-    numSelected: number;
-    onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Vendor) => void;
-    onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    order: Vendor;
-    orderBy: string;
-    rowCount: number;
+  order: Order;
+  orderBy: string;
+  onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Vendor) => void;
 };
 
 
@@ -41,37 +41,37 @@ const headCells: readonly HeadCell[] = [
 ];
   
 
-function EnhancedTableHead({ onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort }: IEnhancedTableProps) {
-    const createSortHandler =
-      (property: keyof Vendor) => (event: React.MouseEvent<unknown>) => {
-        onRequestSort(event, property);
-      };
-  
-    return (
-      <TableHead>
-        <TableRow>
-          {headCells.map((headCell) => (
-            <TableCell
-              key={headCell.id}
-              align={headCell.numeric ? 'right' : 'left'}
-              padding={headCell.disablePadding ? 'none' : 'normal'}
-              sortDirection={orderBy === headCell.id ? order : false}
+function EnhancedTableHead({ order, orderBy, onRequestSort }: IEnhancedTableProps) {
+  const createSortHandler =
+    (property: keyof Vendor) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
             >
-              <TableSortLabel
-                active={orderBy === headCell.id}
-                direction={orderBy === headCell.id ? order : 'asc'}
-                onClick={createSortHandler(headCell.id)}
-              >
-                {headCell.label}
-                {orderBy === headCell.id ? (
-                  <Box component="span" sx={visuallyHidden}>
-                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                  </Box>
-                ) : null}
-              </TableSortLabel>
-            </TableCell>
-          ))}
-        </TableRow>
-      </TableHead>
-    );
-  }
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+};
