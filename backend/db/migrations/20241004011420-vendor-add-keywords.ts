7da4831c626c54a08248ea7922d813f8a943b72a
Aaron Isakov <Aaronisakov@gmail.com>
2024-10-04 19:34:28 -0500

    Fixed migration & seed scripts. DB is now synced up with data from ASD!

--- backend/db/migrations/20241004011420-vendor-add-keywords.ts
+++ backend/db/migrations/20241004011420-vendor-add-keywords.ts
@@ -3,14 +3,14 @@ import {DataTypes, QueryInterface} from "sequelize";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
-    await queryInterface.addColumn("Vendors", "Keywords", {
-      type: Sequelize.STRING,
+    await queryInterface.addColumn("Vendors", "keywords", {
+      type: Sequelize.STRING(2000),
       allowNull: true,
-      unique: true,
+      unique: false,
     });
   },
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async down (queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
-    await queryInterface.removeColumn("Vendors", "Keywords");
+    await queryInterface.removeColumn("Vendors", "keywords");
   }
 };
