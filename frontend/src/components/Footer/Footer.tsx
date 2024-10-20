d8eef2678d1ad7ae186d4ead25eb470ba2277241
Aaron Isakov <Aaronisakov@gmail.com>
2024-10-19 19:13:17 -0500

    Fully implemented the social icons and fixed the footer height. Also made the footer links cleaner and nicer.

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import svg from '../../static/5_SVG-cropped.svg';
 import { LinkedIn, Reddit } from "@mui/icons-material";
-import { Box, SxProps } from "@mui/material";
+import { Box, Link, SxProps, Typography } from "@mui/material";
 
 
 function Footer(): JSX.Element {
@@ -18,20 +18,38 @@ function Footer(): JSX.Element {
         <footer style={{
             position: 'relative',
             bottom: 0,
-            marginTop: '1.5rem',
+            marginTop: '1rem',
             padding: '10px',
-            height: '250px',
+            height: '100%',
             width: '100%',
             background: "#242425",
             color: '#F2E5D1'
         }}>
             <a href={location.origin} target="_self">
-                <img src={svg} alt="Handl Logo" width={"100px"} height={"100px"}/>
+                <img src={svg} alt="Handl Logo" width={"100px"} height={"70px"}/>
             </a>
-            <div><a href={location.origin + "/about-us"} target="_self">About Us</a> / <a href={location.origin + "/contact-us"} target="_self">Contact Us</a> / Privacy Policy</div>
+            <Box sx={{display: 'flex', justifyContent: 'center'}}>
+                <Typography m={1}>
+                    <Link href={location.origin + "/about-us"} target="_self" underline="none" color='#F2E5D1'>
+                        About Us
+                    </Link>
+                </Typography>
+                <Typography m={1}>
+                    <Link href={location.origin + "/contact-us"} target="_self" underline="none" color='#F2E5D1'>
+                        Contact Us
+                    </Link> 
+                </Typography>
+                <Typography m={1}>
+                    Privacy Policy
+                </Typography>
+            </Box>
             <Box aria-label="social-icons-container">
-                <LinkedIn href="https://www.linkedin.com/company/handl/" target="_blank" sx={iconSx}/>
-                <Reddit href="https://www.reddit.com/r/handl/" target="_blank" sx={iconSx}/>
+                <Link href="https://www.linkedin.com/company/handl-technologies/" underline="none" target="_blank" rel="noreferrer" color='#F2E5D1'>
+                    <LinkedIn sx={iconSx}/>
+                </Link>
+                <Link href="https://www.reddit.com/r/handl/" underline="none" target="_blank" rel="noreferrer" color='#F2E5D1'>
+                    <Reddit sx={iconSx}/>
+                </Link>
             </Box>
         </footer>
     )
