7eae2893d9446db904d0628f3f6850df8551e64a
Aaron Isakov <Aaronisakov@gmail.com>
2024-10-23 22:37:36 -0500

    Removed google auth library since it's only meant to be used for server to server authentication and not for frontend framework "server" to server authentication.

--- frontend/src/utils/fetch-wrapper.ts
+++ frontend/src/utils/fetch-wrapper.ts
@@ -1,14 +1,9 @@
-import { GoogleAuth } from "google-auth-library";
-
 const envVariables = process.env;
 const {
     REACT_APP_SERVER_URI,
 } = envVariables;
 
 async function fetchWrapper(route: string, method: string, body?: any): Promise<Response> {
-    const auth = new GoogleAuth();
-    const client = await auth.getIdTokenClient(REACT_APP_SERVER_URI as string);
-    const clientHeaders = await client.getRequestHeaders();
     let httpOptions: RequestInit;
     const httpMethodsWithBody = [
         "POST",
@@ -26,7 +21,6 @@ async function fetchWrapper(route: string, method: string, body?: any): Promise<
             "Access-Control-Origin": window.location.origin,
             "Access-Control-Allow-Origin": window.location.origin,
             "Accept": "application/json",
-            "Authorization": clientHeaders.Authorization,
         },
         redirect: "follow",
         referrerPolicy: "no-referrer",
