13b37345c37b6188d9b5cdb76d63e6fb6191cbcb
Aaron Isakov <Aaronisakov@gmail.com>
2024-10-24 20:00:35 -0500

    Added backed login/verify route because removing it right now would lead to too many breaking changes and right now it's beneficial/serves a purpose in the frontend as an extra safety check for when users will try to get results or any user data.

--- backend/routers/UserRouter.ts
+++ backend/routers/UserRouter.ts
@@ -188,6 +188,35 @@ userRouter.post("/login", async (req: Request, res: Response, next: NextFunction
     }
 });
 
+userRouter.post("/login/verify", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const cookies = req.cookies;
+        const userId: string = req.body.userId;
+        const verificationService: VerificationService = new VerificationService();
+        const isVerified: boolean = await verificationService.VerifyUser(cookies.selector, cookies.validator, userId);
+        if (isVerified) {
+            return res.status(201).send();
+        } else {
+            return res.status(401)
+                .cookie("selector", "", {
+                    maxAge: Number(new Date(1)),
+                    sameSite: "none",
+                    secure: true,
+                    httpOnly: true,
+                })
+                .cookie("validator", "", {
+                    maxAge: Number(new Date(1)),
+                    sameSite: "none",
+                    secure: true,
+                    httpOnly: true,
+                })
+                .send();
+        }
+    } catch (err: unknown) {
+        return next(err as Error);
+    }
+});
+
 userRouter.post("/logout", async (req: Request, res: Response, next: NextFunction) => {
     try {
         const cookies = req.cookies;
