ad2b49e36801267f879d04c4fc61e0b05b597fea
Aaron Isakov <Aaronisakov@gmail.com>
2024-12-08 18:17:31 -0500

    eslint fixes.

--- backend/db/seeders/20240618233029-seed-categories.ts
+++ backend/db/seeders/20240618233029-seed-categories.ts
@@ -1,36 +1,38 @@
 import path from "path";
 import {DataTypes, QueryInterface} from "sequelize";
-import { Category } from "../models/Category";
+import {Category} from "../models/Category";
 const fs = require("node:fs/promises");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     // eslint-disable-next-line
   async up (queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
-    await queryInterface.bulkDelete("Categories", {}, {});
-    let data: { [category: string]: { subcategories: string[] } } = JSON.parse(await fs.readFile(path.resolve(__dirname, "./seed-data/updated-categories.json"), 'utf8'));
-    let formatedData: {
+        await queryInterface.bulkDelete("Categories", {}, {});
+        const data: { [category: string]: { subcategories: string[] } } = JSON.parse(
+            await fs.readFile(path.resolve(__dirname, "./seed-data/updated-categories.json"), "utf8")
+        );
+        const formatedData: {
         category: string;
         subcategory: string;
     }[] = [];
-    Object.keys(data).forEach((category: string) => {
-        data[category].subcategories.forEach((subcategory: string, index: number) => {
-            formatedData.push({
-                category: category,
-                subcategory: subcategory,
-            })
+        Object.keys(data).forEach((category: string) => {
+            data[category].subcategories.forEach((subcategory: string) => {
+                formatedData.push({
+                    category: category,
+                    subcategory: subcategory,
+                });
+            });
         });
-    });
-    for (let i = 0; i < formatedData.length; i++) {
-        await Category.create({
-            category: formatedData[i].category,
-            subcategory: formatedData[i].subcategory,
-        });
-    };
-    return
-  },
+        for (let i = 0; i < formatedData.length; i++) {
+            await Category.create({
+                category: formatedData[i].category,
+                subcategory: formatedData[i].subcategory,
+            });
+        }
+        return;
+    },
     // eslint-disable-next-line
   async down (queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
-    return await queryInterface.bulkDelete("Categories", {}, {});
-  },
+        return await queryInterface.bulkDelete("Categories", {}, {});
+    },
 };
