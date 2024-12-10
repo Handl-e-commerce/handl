f807662078581f8e7af85fdf8e7f65099be0cae6
Aaron Isakov <Aaronisakov@gmail.com>
2024-12-09 21:01:18 -0500

    Refactored home to use less code and be simpler.

--- frontend/src/routes/Home/Home.tsx
+++ frontend/src/routes/Home/Home.tsx
@@ -40,7 +40,7 @@ function Home(): JSX.Element {
     async function getCategories(): Promise<void> {
         const response = await fetchWrapper('/vendors/categories', 'GET');
         const data: { category: string }[] = (await response.json()).result;
-        let breakpoint = 15;
+        let breakpoint = 13;
         let numTranches = Math.ceil(data.length / breakpoint);
         let column: JSX.Element[] = [];
         let matrix: JSX.Element[][] = [];
@@ -102,6 +102,47 @@ function Home(): JSX.Element {
         );
     };
 
+    function createHomePageCategories(childText: string): JSX.Element {
+        return (
+            <Grid container spacing={'5px'} mb={'24px'} data-testid="featured-categories-container" sx={styles.gridContainer}>
+                <Grid item xs={12}>
+                    <Typography 
+                            variant={styles.font.heading.variant}
+                            component={styles.font.heading.component}
+                            fontSize={styles.font.heading.fontSize}
+                            fontWeight={styles.font.heading.fontWeight}
+                        >
+                            {childText}
+                        </Typography>
+                </Grid>
+                <Grid item xs={styles.grid.xs}>
+                    <a href={location.origin + '/results?category=Amazon+FBA+Suppliers'} target="_self">
+                        <img src={fba} alt="" style={styles.image} />
+                    </a>
+                    <Typography variant={'h6'} component={'div'} fontSize={'20px'} textAlign={'center'}>Amazon FBA Suppliers</Typography>
+                </Grid>
+                <Grid item xs={styles.grid.xs}>
+                    <a href={location.origin + '/results?category=Health%2C+Beauty+%26+Wellness'} target="_self">
+                        <img src={health} alt="" style={styles.image} />
+                    </a>
+                    <Typography variant={'h6'} component={'div'} fontSize={'20px'} textAlign={'center'}>Health, Beauty, & Wellness</Typography>
+                </Grid>
+                <Grid item xs={styles.grid.xs}>
+                    <a href={location.origin + `/results?category=Apparel+%2F+Clothing`} target="_self">
+                        <img src={clothing} alt="" style={styles.image} />
+                    </a>
+                    <Typography variant={'h6'} component={'div'} fontSize={'20px'} textAlign={'center'}>Apparel / Clothing</Typography>
+                </Grid>
+                <Grid item xs={styles.grid.xs}>
+                    <a href={location.origin + '/results?category=Electronics'} target="_self">
+                        <img src={electronics} alt="" style={styles.image} />
+                    </a>
+                    <Typography variant={'h6'} component={'div'} fontSize={'20px'} textAlign={'center'}>Electronics</Typography>
+                </Grid>
+            </Grid>
+        );
+    };
+
     useEffect(() => {
         let ignore = false;
         if (!ignore) {
@@ -116,78 +157,8 @@ function Home(): JSX.Element {
         <Box sx={{minHeight: '60rem', textAlign: isMobile ? 'center' : 'left', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <Banner />
             <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                <Grid container spacing={"5px"} mb={'24px'} data-testid="most-viewed-categories-container" sx={styles.gridContainer}>
-                    <Grid item xs={12}>
-                        <Typography 
-                            variant={styles.font.heading.variant}
-                            component={styles.font.heading.component}
-                            fontSize={styles.font.heading.fontSize}
-                            fontWeight={styles.font.heading.fontWeight}
-                        >
-                            Most Viewed Categories
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={styles.grid.xs}>
-                        <a href={location.origin + '/results?search-params=fba'} target="_self">
-                            <img src={fba} alt="" style={styles.image} />
-                        </a>
-                        <Typography variant={'h6'} component={'div'} fontSize={'20px'} textAlign={'center'}>Amazon FBA</Typography>
-                    </Grid>
-                    <Grid item xs={styles.grid.xs}>
-                        <a href={location.origin + '/results?categories=Cosmetics+%2F+Nails+%2F+Hair+Products'} target="_self">
-                            <img src={health} alt="" style={styles.image} />
-                        </a>
-                        <Typography variant={'h6'} component={'div'} fontSize={'20px'} textAlign={'center'}>Health & Beauty</Typography>
-                    </Grid>
-                    <Grid item xs={styles.grid.xs}>
-                        <a href={location.origin + `/results?categories=Hats+%2F+Scarves%2CKids+%2F+Baby+Apparel%2CKids+%2F+Baby+Footwear%2CMen's+Apparel%2CMen's+Footwear%2CUndergarments+%2F+Hosiery+%2F+Socks%2CWomen's+Apparel%2CWomen's+Footwear`} target="_self">
-                            <img src={clothing} alt="" style={styles.image} />
-                        </a>
-                        <Typography variant={'h6'} component={'div'} fontSize={'20px'} textAlign={'center'}>Clothing</Typography>
-                    </Grid>
-                    <Grid item xs={styles.grid.xs}>
-                        <a href={location.origin + '/results?categories=Cameras+%2F+Tablets+%2F+Drones+%2F+MP3+Players+%2F+Home+%2F+Entertainment+Audio+etc.%2CCell+Phone+Accessories+%2F+Wearables+%2F+Headphones+%2F+Speakers'} target="_self">
-                            <img src={electronics} alt="" style={styles.image} />
-                        </a>
-                        <Typography variant={'h6'} component={'div'} fontSize={'20px'} textAlign={'center'}>Electronics</Typography>
-                    </Grid>
-                </Grid>
-                <Grid container spacing={'5px'} mb={'24px'} data-testid="featured-categories-container" sx={styles.gridContainer}>
-                    <Grid item xs={12}>
-                    <Typography 
-                            variant={styles.font.heading.variant}
-                            component={styles.font.heading.component}
-                            fontSize={styles.font.heading.fontSize}
-                            fontWeight={styles.font.heading.fontWeight}
-                        >
-                            Featured Categories
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={styles.grid.xs}>
-                        <a href={location.origin + '/results?search-params=fba'} target="_self">
-                            <img src={fba} alt="" style={styles.image} />
-                        </a>
-                        <Typography variant={'h6'} component={'div'} fontSize={'20px'} textAlign={'center'}>Amazon FBA</Typography>
-                    </Grid>
-                    <Grid item xs={styles.grid.xs}>
-                        <a href={location.origin + '/results?categories=Cosmetics+%2F+Nails+%2F+Hair+Products'} target="_self">
-                            <img src={health} alt="" style={styles.image} />
-                        </a>
-                        <Typography variant={'h6'} component={'div'} fontSize={'20px'} textAlign={'center'}>Health & Beauty</Typography>
-                    </Grid>
-                    <Grid item xs={styles.grid.xs}>
-                        <a href={location.origin + `/results?categories=Hats+%2F+Scarves%2CKids+%2F+Baby+Apparel%2CKids+%2F+Baby+Footwear%2CMen's+Apparel%2CMen's+Footwear%2CUndergarments+%2F+Hosiery+%2F+Socks%2CWomen's+Apparel%2CWomen's+Footwear`} target="_self">
-                            <img src={clothing} alt="" style={styles.image} />
-                        </a>
-                        <Typography variant={'h6'} component={'div'} fontSize={'20px'} textAlign={'center'}>Clothing</Typography>
-                    </Grid>
-                    <Grid item xs={styles.grid.xs}>
-                        <a href={location.origin + '/results?categories=Cameras+%2F+Tablets+%2F+Drones+%2F+MP3+Players+%2F+Home+%2F+Entertainment+Audio+etc.%2CCell+Phone+Accessories+%2F+Wearables+%2F+Headphones+%2F+Speakers'} target="_self">
-                            <img src={electronics} alt="" style={styles.image} />
-                        </a>
-                        <Typography variant={'h6'} component={'div'} fontSize={'20px'} textAlign={'center'}>Electronics</Typography>
-                    </Grid>
-                </Grid>
+                {createHomePageCategories("Most Viewed Categories")}
+                {createHomePageCategories("Featured Categories")}
                 {createCategoriesList()}
             </Container>
         </Box>
