4db369ce14c0e9f4ca33d12636abcdf4bdb4ab97
Aaron Isakov <Aaronisakov@gmail.com>
2024-12-10 22:33:11 -0500

    Fixed broken unit tests and added single test case for subcategories.

--- backend/tests/VendorService.test.ts
+++ backend/tests/VendorService.test.ts
@@ -4,63 +4,70 @@ import {VendorService} from "../services/VendorService";
 describe("VendorService tests", () => {
     it("Should return all results", async () => {
         let vendorService = new VendorService();
-        let results = await vendorService.GetVendors(undefined, undefined, undefined);
+        let results = await vendorService.GetVendors(undefined, undefined, undefined, undefined);
         expect(results).toBeDefined();
         expect(results.length).toBeGreaterThan(0);
     });
 
-    it("Should return only rows that match the passed categories", async () => {
+    it("Should return only rows that match the passed category", async () => {
         let vendorService = new VendorService();
-        let results = await vendorService.GetVendors(["Sunglasses / Eyewear"], undefined, undefined);
+        let results = await vendorService.GetVendors("Apparel / Clothing", undefined, undefined, undefined);
         expect(results).toBeDefined();
-        expect(results.length).toEqual(32);
+        expect(results.length).toEqual(117);
+    });
+
+    it("Should return only rows that match the passed category and subcategories", async () => {
+        let vendorService = new VendorService();
+        let results = await vendorService.GetVendors("Apparel / Clothing", ["Women's Apparel", "Men's Apparel"], undefined, undefined);
+        expect(results).toBeDefined();
+        expect(results.length).toEqual(10);
     });
 
     it("Should return only rows that match the search param", async () => {
         let vendorService = new VendorService();
-        let results = await vendorService.GetVendors(undefined, "365 Fashions Inc.", undefined);
+        let results = await vendorService.GetVendors(undefined, undefined, "365 Fashions Inc.", undefined);
         expect(results).toBeDefined();
         expect(results.length).toEqual(1);
     });
 
     it("Should return only rows that match both search params and passed categories", async () => {
         let vendorService = new VendorService();
-        let results = await vendorService.GetVendors(["Sunglasses / Eyewear"], "365 Fashions Inc.", undefined);
+        let results = await vendorService.GetVendors("Sunglasses & Eyewear", undefined, "365 Fashions Inc.", undefined);
         expect(results).toBeDefined();
         expect(results.length).toEqual(1);
     });
 
     it("Should return only rows that match the passed in states", async () => {
         let vendorService = new VendorService();
-        let results = await vendorService.GetVendors(undefined, undefined, ["NY", "TX"]);
+        let results = await vendorService.GetVendors(undefined, undefined, undefined, ["NY", "TX"]);
         expect(results).toBeDefined();
         expect(results.length).toEqual(158);
     });
     
     it("Should be case insensitive when querying states", async () => {
         let vendorService = new VendorService();
-        let results = await vendorService.GetVendors(undefined, undefined, ["ny", "tx"]);
+        let results = await vendorService.GetVendors(undefined, undefined, undefined, ["ny", "tx"]);
         expect(results).toBeDefined();
         expect(results.length).toEqual(158);
     });
 
     it("Should return only rows that match the search param and states", async () => {
         let vendorService = new VendorService();
-        let results = await vendorService.GetVendors(undefined, "365 Fashions Inc.", ["NY"]);
+        let results = await vendorService.GetVendors(undefined, undefined, "365 Fashions Inc.", ["NY"]);
         expect(results).toBeDefined();
         expect(results.length).toEqual(1);
     });
     
     it("Should return only rows that match the categories, search param, and states", async () => {
         let vendorService = new VendorService();
-        let results = await vendorService.GetVendors(["Sunglasses / Eyewear"], "365 Fashions Inc.", ["NY"]);
+        let results = await vendorService.GetVendors("Sunglasses & Eyewear", undefined, "365 Fashions Inc.", ["NY"]);
         expect(results).toBeDefined();
         expect(results.length).toEqual(1);
     });
 
     it("Should return values that are specific to keywords", async () => {
         let vendorService = new VendorService();
-        let results = await vendorService.GetVendors(undefined, "fba", undefined);
+        let results = await vendorService.GetVendors(undefined, undefined, "fba", undefined);
         expect(results).toBeDefined();
         expect(results.length).toEqual(4);
     });
