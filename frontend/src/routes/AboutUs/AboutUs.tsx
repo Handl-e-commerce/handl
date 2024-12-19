3e7b7d0b6202fd6b2d57a7965d7c69e44dd2d8cb
Aaron Isakov <Aaronisakov@gmail.com>
2024-12-18 19:22:04 -0500

    Made about us look nicer.

--- frontend/src/routes/AboutUs/AboutUs.tsx
+++ frontend/src/routes/AboutUs/AboutUs.tsx
@@ -3,9 +3,9 @@ import { Box, Typography } from "@mui/material";
 
 function AboutUs(): JSX.Element {
     return (
-        <Box style={{height: '100%', display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+        <Box style={{height: '35rem', display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
             <Box sx={{width: '90%'}}>
-                <Typography variant="h4" component='h4'>Handl: Revolutionizing Wholesale E-Commerce</Typography>
+                <Typography variant="h4" component='h4' sx={{margin: '14px'}}>Handl: Revolutionizing Wholesale E-Commerce</Typography>
                 <Typography variant="body1" component='p' sx={{margin: '14px'}}>At Handl, we envision a world where the e-commerce industry is transparent, efficient, and accessible to all. We believe the current market is plagued by inefficiencies, gate-keeping, and costly trade shows as the primary means of discovering new inventory sources. Our mission is to dismantle these barriers, democratizing information to seamlessly connect retailers with wholesalers.</Typography>
                 <Typography variant="body1" component='p' sx={{margin: '14px'}}>Handl is your ultimate destination for sourcing inventory, whether you're looking for FBA sellers, general merchandise, or cosmetics. We are dedicated to simplifying the process for underserved e-commerce retailers, ensuring that finding new suppliers is no longer a daunting task.</Typography>
                 <Typography variant="body1" component='p' sx={{margin: '14px'}}>Experience a new era of e-commerce with Handl.</Typography>
