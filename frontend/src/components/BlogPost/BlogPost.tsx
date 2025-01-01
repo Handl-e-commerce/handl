8010dac34e88a5ec24268cc8f55d58376c85af93
Aaron Isakov <Aaronisakov@gmail.com>
2025-01-01 17:42:19 -0500

    Improved styling for blog post component.

--- frontend/src/components/BlogPost/BlogPost.tsx
+++ frontend/src/components/BlogPost/BlogPost.tsx
@@ -1,15 +1,18 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import { useNavigate, useOutletContext } from "react-router-dom";
 import { HowToFindReliableWholesalers } from "./BlogPosts/HowToFindReliableWholeSalers";
 import { FiveWaysToStreamlineInventoryManagement } from "./BlogPosts/FiveWaysToStreamlineInventoryManagement";
 import { EightCommonSourcingMistakes } from "./BlogPosts/EightCommonSourcingMistakes";
 import { CaseStudy } from "./BlogPosts/CaseStudy";
+import { ArrowBack } from "@mui/icons-material";
+import { useMobile } from "../../hooks/useMobile";
 
 function BlogPost(): JSX.Element {
-    window.scrollTo({
-        top: 0
-    });
+    // window.scrollTo({
+    //     top: 0
+    // });
     const navigate = useNavigate();
+    const isMobille = useMobile();
     const [currentBlog, index] = useOutletContext<any>();
     const blogPosts = [
         HowToFindReliableWholesalers,
@@ -20,18 +23,37 @@ function BlogPost(): JSX.Element {
 
     return (
         <Box>
-            <Box aria-label="Blog Header" sx={{ 
+            <Grid
+                container
+                aria-label="Blog Header" 
+                sx={{ 
                     background: '#2F2F30',
                     color: '#f2e5d2',
-                    height: '200px',
                     marginBottom: '2rem',
                     display: 'flex',
-                    flexDirection: 'column'
+                    flexDirection: 'column',
+                    padding: '2%'
                 }}
             >
-                <Typography onClick={() => navigate("/blog")} textAlign={'left'} sx={{ cursor: "pointer" }}>Go Back</Typography>
-                <Typography variant="h4">{currentBlog.title}</Typography>
-            </Box>
+                <Grid item xs={12}>
+                    <Typography
+                        onClick={() => navigate("/blog")} 
+                        textAlign={'left'} 
+                        sx={{ cursor: "pointer" }}
+                        display={'flex'}
+                        alignItems={'center'}
+                        fontWeight={600}
+                        fontSize={18}
+                        width={"fit-content"}
+                    >
+                        <ArrowBack sx={{marginRight: '.25rem'}}/>
+                        Go Back
+                    </Typography>
+                </Grid>
+                <Grid item marginY={'20px'} xs={6}>
+                    <Typography fontSize={'48px'} fontWeight={700}>{currentBlog.title}</Typography>
+                </Grid>
+            </Grid>
             <Box aria-label="Blog Body">
                 {blogPosts[index]}
             </Box>
