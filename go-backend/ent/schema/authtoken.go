6f8262fe9e63c2010a496275b836b6e51b632977
Aaron Isakov <Aaronisakov@gmail.com>
2025-05-16 23:39:18 -0500

    Ran ent generate command to generate my schemas. Commented out code in authtoken temporarily for future refactor.

--- go-backend/ent/schema/authtoken.go
+++ go-backend/ent/schema/authtoken.go
@@ -2,9 +2,9 @@ package schema
 
 import (
     "entgo.io/ent"
-    "entgo.io/ent/schema/edge"
-    "entgo.io/ent/schema/field"
-    "time"
+    // "entgo.io/ent/schema/edge"
+    // "entgo.io/ent/schema/field"
+    // "time"
 )
 // AuthToken holds the schema definition for the AuthToken entity.
 type AuthToken struct {
@@ -13,37 +13,39 @@ type AuthToken struct {
 
 // Fields of the AuthToken.
 func (AuthToken) Fields() []ent.Field {
-	return []ent.Field{
-        field.Int("id").
-            Positive().
-            Unique().
-            Immutable(),
+	return nil
+	// return []ent.Field{
+    //     field.Int("id").
+    //         Positive().
+    //         Unique().
+    //         Immutable(),
 
-        field.String("selector").
-            NotEmpty(),
+    //     field.String("selector").
+    //         NotEmpty(),
 
-        field.String("validator").
-            NotEmpty(),
+    //     field.String("validator").
+    //         NotEmpty(),
 
-        field.Time("expires").
-            NotEmpty(),
+    //     field.Time("expires").
+    //         NotEmpty(),
 
-        field.Time("created_at").
-            Default(time.Now),
+    //     field.Time("created_at").
+    //         Default(time.Now),
 
-        field.Time("updated_at").
-            Default(time.Now).
-            UpdateDefault(time.Now),
-    }
+    //     field.Time("updated_at").
+    //         Default(time.Now).
+    //         UpdateDefault(time.Now),
+    // }
 }
 
 // Edges of the AuthToken.
 func (AuthToken) Edges() []ent.Edge {
-	return []ent.Edge{
-        edge.From("user", User.Type).
-            Ref("auth_token").
-            Unique().
-            Required().
-            Field("user_uuid"),
-    }
+	return nil
+	// return []ent.Edge{
+    //     edge.From("user", User.Type).
+    //         Ref("auth_token").
+    //         Unique().
+    //         Required().
+    //         Field("user_uuid"),
+    // }
 }
--- go-backend/go.mod
+++ go-backend/go.mod
@@ -2,6 +2,9 @@ module handl-server
 
 go 1.24.2
 
-require github.com/go-chi/chi/v5 v5.2.1
+require (
+	github.com/go-chi/chi/v5 v5.2.1
+	github.com/google/uuid v1.3.0
+)
 
-require entgo.io/ent v0.14.4 // indirect
+require entgo.io/ent v0.14.4
