6a6eb322a17edc8f83fa990ae735d531bc7d80f6
Aaron Isakov <Aaronisakov@gmail.com>
2025-05-25 19:21:26 -0500

    Created empty migration file.

---
+++ backend/db/migrations/20250525152656-user-add-type.ts
@@ -0,0 +1,22 @@
+import {DataTypes, QueryInterface} from "sequelize";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down (queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
