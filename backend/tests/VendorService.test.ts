55a38850f5c5fd97a4cff7a402f78981edccb98c
Aaron Isakov <Aaronisakov@gmail.com>
2025-06-14 18:03:20 -0500

    Removed unused reference.

--- backend/tests/VendorService.test.ts
+++ backend/tests/VendorService.test.ts
@@ -2,7 +2,6 @@ import {describe, expect, it} from "@jest/globals";
 import {VendorService} from "../services/VendorService";
 const express = require("express");
 const request = require("supertest");
-import {vendorRouter} from "../routers/VendorRouter";
 
 describe("VendorService tests", () => {
     it.skip("Should return all results", async () => {
@@ -39,18 +38,5 @@ describe("VendorService tests", () => {
         let results = await vendorService.GetVendors(undefined, undefined, ["ny", "tx"], undefined);
         expect(results).toBeDefined();
         expect(results.length).toEqual(237);
-    });
-
-    it("Should return all vendor categories from the /vendors/categories endpoint", async () => {
-
-        const app = express();
-        app.use(express.json());
-        app.use("/vendors", vendorRouter);
-
-        const response = await request(app).get("/vendors/categories");
-        expect(response.status).toBe(200);
-        expect(Array.isArray(response.body)).toBe(true);
-        expect(response.body.length).toBeGreaterThan(0);
-        expect(response.body).toContain("Apparel / Clothing"); // Adjust to match a known category
     });
 });
